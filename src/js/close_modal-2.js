d243a11973d4509a3d8d35c91968db76087f4649
Gregory0401 <grigoriosdart@gmail.com>
2022-08-18 17:16:35 +0300

    modal 2.0

---
+++ src/js/close_modal-2.js
@@ -0,0 +1,12 @@
+(() => {
+    const refs = {
+      backdrop: document.querySelector('[data-modal-2]'),
+      closeBtn: document.querySelector('[data-modal-close-2]'),
+    };
+  
+    refs.closeBtn.addEventListener('click', closeModal);
+  
+    function closeModal() {
+      refs.backdrop.classList.add('is-hidden-2');
+    }
+  })();
