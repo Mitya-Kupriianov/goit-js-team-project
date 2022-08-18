bbd225272373399b9de61856ca882dd7a912d332
Max Kryzhanovskyi <maximkryzhanovsky@gmail.com>
2022-08-18 04:34:37 +0300

    end of 1 day

---
+++ src/index.js
@@ -0,0 +1,60 @@
+// // let source = ;
+// const template = Handlebars.compile(
+//   "{{#each letters}}<li class='hero__item hero__item--{{this}} grid__item'></li><a href='#' class='grid__link'>{{letter}}</a>{{/each}}'"
+// );
+
+// const refs = {
+//   heroList: document.querySelector('.hero__list'),
+// };
+// const { heroList } = refs;
+// console.log(refs);
+
+// const letters = {
+//   letter: [
+//     'A',
+//     'B',
+//     'C',
+//     'D',
+//     'E',
+//     'F',
+//     'G',
+//     'H',
+//     'I',
+//     'J',
+//     'K',
+//     'L',
+//     'M',
+//     'N',
+//     'O',
+//     'P',
+//     'Q',
+//     'R',
+//     'S',
+//     'T',
+//     'U',
+//     'V',
+//     'W',
+//     'X',
+//     'Y',
+//     'Z',
+//     '1',
+//     '2',
+//     '3',
+//     '4',
+//     '5',
+//     '6',
+//     '7',
+//     '8',
+//     '9',
+//     '0',
+//   ],
+// };
+
+// function createMarkup(letters) {
+//   console.log(letters);
+//   //   const markup = template(letters);
+//   return heroList.insertAdjacentHTML('beforeend', template(letters));
+// }
+
+// createMarkup(heroList);
+// // console.log(markup);
---
+++ src/js-hero/render.js
@@ -0,0 +1,15 @@
+// import template from '../hero_hbs/letters.hbs';
+// const source = document.getElementById('entry-template').innerHTML;
+
+
+//   let template = Handlebars.compile(
+//     document.querySelector('#letters').innerHTML
+//   );
+//   const filled = template(data, {
+//     noEscape: true,
+//   });
+//   document.querySelector('.hero__list').insertAdjacentHTML(filled);
+
+// Handlebars.registerHelper('letters', function (string) {
+//   return string;
+// });
