7dc8789e15303dcbb5b326d467e1fcd6921aa9ab
Mitya-Kupriianov <106350869+Mitya-Kupriianov@users.noreply.github.com>
2022-08-18 18:53:28 +0300

    js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './js/open-close-mob-menu';
 import './js/render';
+import './js/close_modal-coctail';
+import './js/close_modal-components';
+import './js/firebase-config';
+import './js/transform_modal-coctail';
+import './js/transform_modal-components';
---
+++ src/js/firebase-config.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use // https://firebase.google.com/docs/web/setup#available-libraries  // Your web app's Firebase configuration const firebaseConfig = {   apiKey: "AIzaSyAULeMb5TUpp9pfALatcfwqThVI7Yd4YVI",   authDomain: "cocktails-39b57.firebaseapp.com",   projectId: "cocktails-39b57",   storageBucket: "cocktails-39b57.appspot.com",   messagingSenderId: "894388404677",   appId: "1:894388404677:web:5aeeaa2079f89382130bac" };  // Initialize Firebase const app = initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAULeMb5TUpp9pfALatcfwqThVI7Yd4YVI',
+  authDomain: 'cocktails-39b57.firebaseapp.com',
+  projectId: 'cocktails-39b57',
+  storageBucket: 'cocktails-39b57.appspot.com',
+  messagingSenderId: '894388404677',
+  appId: '1:894388404677:web:5aeeaa2079f89382130bac',
+};
