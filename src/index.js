ec94d6737ea0283c46ac5dfe35e7ef190a0c774b
Max Kryzhanovskyi <maximkryzhanovsky@gmail.com>
2022-08-18 12:17:13 +0300

    bugs

--- src/index.js
+++ src/index.js
@@ -1,60 +1,58 @@
-// // let source = ;
-// const template = Handlebars.compile(
-//   "{{#each letters}}<li class='hero__item hero__item--{{this}} grid__item'></li><a href='#' class='grid__link'>{{letter}}</a>{{/each}}'"
-// );
+import templateFunction from './hero_hbs/letters.hbs';
+console.log(templateFunction);
 
-// const refs = {
-//   heroList: document.querySelector('.hero__list'),
-// };
-// const { heroList } = refs;
-// console.log(refs);
+const refs = {
+  heroList: document.querySelector('.hero__list'),
+};
+const { heroList } = refs;
+console.log(heroList);
 
-// const letters = {
-//   letter: [
-//     'A',
-//     'B',
-//     'C',
-//     'D',
-//     'E',
-//     'F',
-//     'G',
-//     'H',
-//     'I',
-//     'J',
-//     'K',
-//     'L',
-//     'M',
-//     'N',
-//     'O',
-//     'P',
-//     'Q',
-//     'R',
-//     'S',
-//     'T',
-//     'U',
-//     'V',
-//     'W',
-//     'X',
-//     'Y',
-//     'Z',
-//     '1',
-//     '2',
-//     '3',
-//     '4',
-//     '5',
-//     '6',
-//     '7',
-//     '8',
-//     '9',
-//     '0',
-//   ],
-// };
+const letters = {
+  letter: [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0',
+  ],
+};
 
-// function createMarkup(letters) {
-//   console.log(letters);
-//   //   const markup = template(letters);
-//   return heroList.insertAdjacentHTML('beforeend', template(letters));
-// }
+console.log(letters);
+function createMarkup(letters) {
+  const markup = templateFunction(letters);
+  return heroList.insertAdjacentHTML('beforeend', markup);
+}
 
-// createMarkup(heroList);
-// // console.log(markup);
+createMarkup(letters);
+console.log(templateFunction(letters));
