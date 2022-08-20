d458a1f0d76c2bd42f60f2ae32bcc2529c69d1a8
WoLfEER <101401666+WoLfEER@users.noreply.github.com>
2022-08-20 12:58:57 +0300

    footer fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './js/open-close-mob-menu';
 import './js/render';
-import './js/close_modal-coctail';
-import './js/close_modal-components';
-import './js/firebase-config';
-import './js/transform_modal-coctail';
-import './js/transform_modal-components';
+// import './js/close_modal-coctail';
+// import './js/close_modal-components';
+// import './js/firebase-config';
+// import './js/transform_modal-coctail';
+// import './js/transform_modal-components';
 import './js/team_modal';
 import './js/open-close-footer-modal';
 import './js/service/hero-refs';
--- src/js/darkmode-theme.js
+++ src/js/darkmode-theme.js
@@ -1,22 +1,22 @@
-import Darkmode from 'darkmode-js';
+// import Darkmode from 'darkmode-js';
 
-new Darkmode().showWidget();
+// new Darkmode().showWidget();
 
-const options = {
-  bottom: '100', // default: '32px'
-  right: '32px', // default: '32px'
-  left: '0', // default: 'unset'
-  time: '0.7s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff', // default: '#fff'
-  buttonColorDark: '#414141',
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: false, // default: true,
-  label: '🚀', // default: ''
-  autoMatchOsTheme: true, // default: true
-};
+// const options = {
+//   top: '0', // default: '32px'
+//   right: 'unset', // default: '32px'
+//   left: '132px', // default: 'unset'
+//   time: '0.5s', // default: '0.3s'
+//   mixColor: '#fff', // default: '#fff'
+//   backgroundColor: '#fff', // default: '#fff'
+//   buttonColorDark: '#100f2c', // default: '#100f2c'
+//   buttonColorLight: '#fff', // default: '#fff'
+//   saveInCookies: false, // default: true,
+//   label: '🌓', // default: ''
+//   autoMatchOsTheme: true, // default: true
+// };
 
-console.log(options);
+// console.log(options);
 
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
+// const darkmode = new Darkmode(options);
+// darkmode.showWidget();
