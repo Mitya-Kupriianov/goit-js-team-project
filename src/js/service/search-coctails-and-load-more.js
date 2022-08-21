bb7e15babe2ed5eb3c9742121d24bcd2d96d283a
Mitya-Kupriianov <MityaBlow@gmail.com>
2022-08-21 20:50:31 +0300

    Update search-coctails-and-load-more.js

--- src/js/service/search-coctails-and-load-more.js
+++ src/js/service/search-coctails-and-load-more.js
@@ -1,9 +1,9 @@
-import { getRandomCocktail } from './getCocktail';
+import CocktailAPI from './getCocktail';
 
 export default async function insertCocktails() {
   let array = [];
   for (let index = 1; index <= 9; index++) {
-    const element = await getRandomCocktail();
+    const element = await CocktailAPI.getRandomCocktail();
     array.push(element.data.drinks);
   }
 }
