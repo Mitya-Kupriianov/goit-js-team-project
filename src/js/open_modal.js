3a968256a8642c1dd83be9d2213697aad91e0f4a
Max Kryzhanovskyi <maximkryzhanovsky@gmail.com>
2022-08-21 13:35:44 +0300

    Gregoriy modal +

--- src/js/open_modal.js
+++ src/js/open_modal.js
@@ -1,14 +1,9 @@
 const refs = {
   modalOpenBtn: document.querySelectorAll('[data-modal-cocktail-open]'),
-
   backdrop: document.querySelector('[data-modal]'),
 };
-
-//listeners //
-refs.modalOpenBtn.addEventListener('click', modalCocktOpen);
-
-function modalCocktOpen(event) {
-  console.log(event);
-  event.preventDefault();
-  refs.backdrop.classList.remove('is-hidden-modal-coctails');
-}
+refs.modalOpenBtn.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    refs.backdrop.classList.remove('is-hidden-modal-coctails');
+  });
+});
