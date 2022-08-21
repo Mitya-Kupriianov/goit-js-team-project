8ad1c168a9a1382e82d79fb808c0788d7c72317d
Mitya-Kupriianov <MityaBlow@gmail.com>
2022-08-21 20:40:23 +0300

    fix js

--- src/index.js
+++ src/index.js
@@ -18,10 +18,9 @@ import './js/service/index';
 import './js/service/select';
 
 import './js/service/getCocktail';
-import './js/service/search coctails and load more';
+import './js/service/search-coctails-and-load-more';
 
 import './js/render-random-cards-to main';
 import './js/searchByLetter';
-import './js/service/search coctails and load more';
 import './js/service/create-markup';
 import './js/service/render-markup';
