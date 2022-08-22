106e963efd14eb64e78f97972ea90aea30e7f678
Mitya-Kupriianov <106350869+Mitya-Kupriianov@users.noreply.github.com>
2022-08-22 23:47:05 +0300

    Update render-markup.js

--- src/js/service/render-markup.js
+++ src/js/service/render-markup.js
@@ -3,8 +3,6 @@ import {
   createRandomMarkup,
   renderMarkup,
   markupFilter,
-  // addEvents,
-  addFavoriteEvents,
 } from './create-markup';
 
 const cocktailList = document.querySelector('.cocktails__list');
@@ -13,14 +11,9 @@ const cocktailAPI = new CocktailAPI();
 export async function createAndRenderRandomMarkup() {
   const array = await cocktailAPI.getRandomCocktail();
 
-  //const markup = createRandomMarkup(array);
-  //const filteredMarkup = markupFilter(markup);
-  //renderMarkup(cocktailList, filteredMarkup);
-
-  const markup = createRandomMarkup(arr);
-  renderMarkup(cocktailList, markup);
-  // addEvents();
-  addFavoriteEvents();
+  const markup = createRandomMarkup(array);
+  const filteredMarkup = markupFilter(markup);
+  renderMarkup(cocktailList, filteredMarkup);
 }
 
 window.addEventListener('load', createAndRenderRandomMarkup);
