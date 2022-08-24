07b18f9a81b8a8c4b6dcb1597964bc7930463d31
Max Kryzhanovskyi <maximkryzhanovsky@gmail.com>
2022-08-24 03:25:33 +0300

    commit

---
+++ src/cocktails.js
@@ -0,0 +1,18 @@
+import './js/open-close-mob-menu';
+import './js/close_modal-coctail';
+import './js/close_modal-components';
+import './js/firebase-config';
+import './js/transform_modal-coctail';
+import './js/transform_modal-components';
+import './js/team_modal';
+import './js/open-close-footer-modal';
+import './js/open_modal';
+
+import './js/header';
+import './js/service/dark-mod';
+import './js/service/login';
+import './js/service/index';
+import './js/service/getCocktail';
+import './js/refs';
+import './js/service/localStorage';
+import './js/favorite-cocktails';
