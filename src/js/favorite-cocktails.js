56a543946907f582af7d71817d20232b6a36562a
Mitya-Kupriianov <MityaBlow@gmail.com>
2022-08-26 02:34:04 +0300

    Update favorite-cocktails.js

--- src/js/favorite-cocktails.js
+++ src/js/favorite-cocktails.js
@@ -3,23 +3,21 @@ import { getCocktailStorageData } from '../js/service/localStorage';
 import CocktailAPI from './service/getCocktail';
 import { refs } from './modal_markup';
 
-refs = {
-  favoriteList: document.querySelector('.cocktails__list'),
-};
+const favoriteList = document.querySelector('.cocktails__list');
+
 const favorite = new CocktailAPI();
 
 export async function onFavoriteCocktailsLoad() {
   const data = await getCocktailStorageData(favorite.KEY);
   const drinks = [];
-  const list = document.querySelector('.cocktails__list');
   if (!data) {
-    list.innerHTML = "Sorry, we didn't find any cocktail for you";
+    favoriteList.innerHTML = "Sorry, we didn't find any cocktail for you";
   }
 
   const render = () => {
     const arr = { data: { drinks: drinks } };
     const markup = createMarkup(arr);
-    renderMarkup(list, markup);
+    renderMarkup(favoriteList, markup);
   };
 
   await data?.forEach(async id => {
