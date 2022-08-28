32736f5115d247cf5677aefc4f0e2de18d6d01cf
VadymShylov <shylovvadym@gmail.com>
2022-08-28 14:57:39 +0100

    updated favor-ingr.card

--- src/js/favorite-ing.js
+++ src/js/favorite-ing.js
@@ -51,7 +51,7 @@ export function createFavoriteIngredientsMarkup({
   idIngredient,
 }) {
   return /*html*/ `
-      <li class="favorite__list-item card-set-item">
+      <li class="favorite__list-item cocktails__item card-set-item">
       <p class="favorite__list-name">${strIngredient}</p>
       <p class="favorite__list-type">${strType}</p>
       <div class="favorite__btn-wrap">
